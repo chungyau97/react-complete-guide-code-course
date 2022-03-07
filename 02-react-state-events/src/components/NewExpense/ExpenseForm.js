59c9c22a8ad6e6b29d7531121cbc5928b4f3de79
chungyau97 <chungyau97@gmail.com>
2022-03-07 21:58:10 +0800

    Adding Two-Way Binding

--- 02-react-state-events/src/components/NewExpense/ExpenseForm.js
+++ 02-react-state-events/src/components/NewExpense/ExpenseForm.js
@@ -23,13 +23,20 @@ function ExpenseForm() {
       date: new Date(enteredDate),
     };
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}></input>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -37,6 +44,7 @@ function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           ></input>
         </div>
@@ -46,6 +54,7 @@ function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           ></input>
         </div>
