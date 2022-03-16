329fb099d3f0c9a4c0b31c08f2d960ac928db028
chungyau97 <chungyau97@gmail.com>
2022-03-16 15:01:54 +0800

    Understanding Keys

--- 03-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
+++ 03-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
@@ -18,6 +18,7 @@ function Expenses(props) {
         ></ExpensesFilter>
         {props.items.map((expense) => (
           <ExpenseItem
+          key = {expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
