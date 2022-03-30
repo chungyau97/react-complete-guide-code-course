ea182f3c1331a196f21f6c06e7f13a854f9fd324
chungyau97 <chungyau97@gmail.com>
2022-03-30 13:41:13 +0800

    Adding Conditional Return Statement

--- 03-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
+++ 03-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 function Expenses(props) {
@@ -19,14 +19,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         ></ExpensesFilter>
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          ></ExpenseItem>
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
