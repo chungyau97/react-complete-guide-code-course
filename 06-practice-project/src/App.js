e773c8ea6dbefa9c2593894249a6851150eb0144
chungyau97 <chungyau97@gmail.com>
2022-08-11 20:47:23 +0800

    Adding a Users List Component

--- 06-practice-project/src/App.js
+++ 06-practice-project/src/App.js
@@ -1,8 +1,10 @@
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UsersList";
 function App() {
   return (
     <div>
       <AddUser></AddUser>
+      <UserList users={[]}></UserList>
     </div>
   );
 }
